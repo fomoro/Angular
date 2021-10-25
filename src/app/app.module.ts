b1b226eeb27b25782358d784e3149af4d73c7865
fomoro <fomoro@gmail.com>
2021-10-25 13:45:25 -0500

    se crean componente usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario.component';
+import { FormsModule } from '@angular/forms';
+import { AleatorioComponent } from './aleatorio/aleatorio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, UsuarioComponent, AleatorioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
